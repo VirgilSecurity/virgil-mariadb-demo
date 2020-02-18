1ce77d1e1fa85b58ca145a898a0b824e7b61a1b6
Dmytro Kosolap <42dkosolap@gmail.com>
2020-02-18 13:42:25 +0200

    allow POST

--- server/src/main/java/com/virgilsecurity/demo/purekit/server/ServerApplication.java
+++ server/src/main/java/com/virgilsecurity/demo/purekit/server/ServerApplication.java
@@ -63,7 +63,7 @@ public class ServerApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("*");
-				registry.addMapping("/**").allowedMethods("GET", "PUT");
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
 			}
 		};
 	}
