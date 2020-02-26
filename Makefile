cb82ec8e333f59576059f1352921cb480060a646
Sergey Kandyla <skandyla@gmail.com>
2020-02-26 18:25:47 +0300

    move generate-env to server

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ demo-client:
 	cd client && docker build -t $(IMAGENAME_CLIENT) .
 
 generate-env:
-	./generate-env.sh
+	server/generate-env.sh
 
 build-mvn:
 	cd server && mvn clean package
--- server/generate-env.sh
+++ server/generate-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "generating .env from Travis environment variables for CI tests"
-cat << EOF > server/.env
+cat << EOF > .env
 # generating vars for tests from CI Env variables
 # Application ID
 virgil_appId=${virgil_appId}
